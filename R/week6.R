4a6493c90f2c6aa748eeadf4ac0d6db6e580b201
Saron M Demeke <demek004@umn.edu>
2023-02-25 16:19:14 -0600

    checks on match accuracy with edits

--- R/week6.R
+++ R/week6.R
@@ -16,16 +16,14 @@ sample(citations_txt, size = 10)
 
 citations_tbl <- tibble(line = 1:length(citations_txt), cite = citations_txt) %>% 
    mutate(cite = str_remove_all(cite,pattern=regex("[\"']"))) %>% 
-   mutate(year = str_match(cite,pattern=regex("\\((\\d{4})\\)?\\.?"))[,2]) %>% 
-  # #11,153 cases don't match because year is outside parentheses or not present. not APA so ignore?
-  # #one of matches has 'year' = 5955. ignore for now
-  # #some are (XXXX, month) - matching with \\)?
-   mutate(page_start = str_match(cite,pattern=regex("(\\d+)[-?]\\d+"))[,2]) %>% 
-  # #above won't work if misspecified APA, e.g. page:page. ignore these cases
-  # #captures some wrong numbers from DOI. fix?
+   mutate(year = str_match(cite,pattern=regex("\\((\\d{4})[,\\s\\w+]*\\)\\."))[,2]) %>% 
+   mutate(page_start = str_match(cite,pattern=regex("(?:,|p?p\\.)\\s(\\d+)-\\d+\\)?\\."))[,2]) %>% 
+  # #above won't work if not APA, e.g. page:page or page?page or :page-page.
+  #is :\\spage-page proper APA7? no idea. ignore for now. can add to or()
+  #can something be letter, page-page? dont know but it can be ., page. so shouldn't specify what comes before comma
    mutate(perf_ref = str_detect(cite,pattern=fixed("performance", ignore_case = T))) %>% 
-  # #works well
-   mutate(title = str_match(cite,pattern=regex("\\)\\.?.(.*?)[.?!]\\s?[A-Z]"))[,2]) %>% 
-  #.*? as non-greedy. will take first instance of where .* is followed by [.?!]\\s?[A-Z]
-  mutate(first_author = str_match(cite,pattern=regex("(^.*?)(?:\\.,| \\(| and)"))[,2])
+   mutate(title = str_match(cite,pattern=regex("\\)\\.\\s(.*?)[.?!][^.]*\\."))[,2]) %>%
+  #messes up for website refs that don't end in full stop
+   mutate(first_author = str_match(cite,pattern=regex("(^.*?)(?:\\.,\\s[&[A-Z]]|\\.\\s\\()"))[,2])
 sum(!is.na(citations_tbl$first_author))   
+#fix the 'and' names. and the ones that go on without spaces
